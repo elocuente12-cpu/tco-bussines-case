b19f8b4c8a456105a358fee981882fcfc2623637
Javier Sepulveda <javier.sepulveda@JAVIER-SEPULVEDA.local>
2026-09-01 15:47:15 -0500

    feat: update tco prod

--- digitalenvoy-feature-ami-builder/ami-builder/build/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket               = "eec-aws-us-nada-sharedservices-sandbox-tfstate"
-    workspace_key_prefix = "ami/nada-digital-envoy/digital-envoy/build"
-    key                  = "terraform.tfstate"
-    region               = "us-east-1"
- }
-}
